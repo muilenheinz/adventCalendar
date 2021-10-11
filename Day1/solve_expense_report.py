8ec7b3aa196246ba88e582b5b944805a13eee29e
Tobias Jung <tobias.jung@ise.fraunhofer.de>
2021-10-11 13:00:20 +0200

    Created tests for Day1: solve_expense_report

---
+++ Day1/solve_expense_report.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+target_value = 2020
+
+def loadInput():
+    # TODO
+
+# "Specifically, they need you to find the two entries that sum to target_value and then multiply those two
+# numbers together"
+# @_expsens: Pandas Data Frame of the expense report
+# @_target_value: value the two found numbers should add up to
+def getNumbersAddingUpToTargetValue(_expsens, _target_value):
+    # TODO
+
---
+++ Day1/solve_expense_report_tests.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from solve_expense_report import *
+
+
+# tests the normal case, given as ample solution in the task
+def test_normal():
+    expenses = pd.DataFrame({1721, 979, 366, 299, 675, 1456})
+    assert(getNumbersAddingUpToTargetValue(expenses, 2020) == 514579)
+
+
+# tests what happens, when no two values add up to the target_value
+def test_no_matching_values():
+    expenses = pd.DataFrame({100, 400, 3000})
+    assert(getNumbersAddingUpToTargetValue(expenses, 2020) == -1)
+
+
+# tests what happens when the expense report is empty
+def test_empty_list():
+    expenses = pd.DataFrame()
+    assert(getNumbersAddingUpToTargetValue(expenses, 2020) == -1)
+
+
+# tests what happens, when the solution consists of 2020 and 0
+def test_zero_and_target_value():
+    expenses = pd.DataFrame({2020, 0, 145, 897})
+    assert(getNumbersAddingUpToTargetValue(expenses, 2020) == 0)
+
+
+# execute the tests
+test_normal()
+test_no_matching_values()
+test_empty_list()
+test_zero_and_target_value()
